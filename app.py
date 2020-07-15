0dc31c135b7fd7f482f3bc155a8c8e9746090417
Rakesh-droid <49478556+Rakesh-droid@users.noreply.github.com>
2020-07-15 06:52:48 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -6,27 +6,32 @@ Created on Sun Jul 12 23:12:08 2020
 """
 from flask import Flask, request,jsonify,render_template
 import pickle
+import pandas as pd
+import numpy as np 
 
 app = Flask(__name__)
 filename = 'diabetes-prediction.pkl'
 model = pickle.load(open(filename, 'rb'))
 
 @app.route('/')
-def home():
+def man():
     return render_template('index.html')
 
-@app.route('/predict', methods = ['POST'])
-def predict():
-    '''
-    For rendering results on HTML GUI
-    '''
-    int_features = [int(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
-    
-    output = round(prediction[0], 2)
 
-    return render_template('index.html', prediction_text='your report is  $ {}'.format(output))
-    
+@app.route('/predict', methods=['POST'])
+def home():
+    data1 = request.form['a']
+    data2 = request.form['b']
+    data3 = request.form['c']
+    data4 = request.form['d']
+    data5 = request.form['e']
+    data6 = request.form['f']
+    data7 = request.form['g']
+    data8 = request.form['h']
+    arr = np.array([[data1, data2, data3, data4,data5,data6,data7,data8]])
+    pred = model.predict(arr)
+    return render_template('after.html',data=pred)
+
+
 if __name__ == "__main__":
-    app.run(debug =True)
+    app.run(debug=True)
